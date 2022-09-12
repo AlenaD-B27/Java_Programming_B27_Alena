b62f572eabdf51377c600d3d6fa23aafb6766960
AlenaD-B27 <lenador.18@gmail.com>
2022-09-11 21:50:30 -0500

    abstraction example 2 ArrayList is added

--- src/day42_abstraction/person/WorkPlace.java
+++ src/day42_abstraction/person/WorkPlace.java
@@ -1,5 +1,8 @@
 package day42_abstraction.person;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class WorkPlace {
     public static void main(String[] args) {
 
@@ -13,6 +16,23 @@ public class WorkPlace {
         System.out.println(baz);
         baz.sleep(8);
         baz.work();
+
+        ArrayList <Tester> testers = new ArrayList<>();
+        testers.add(baz);
+        testers.addAll(Arrays.asList(
+                new Tester("Bato", 30, "SDET", 150_000),
+                new Tester("Alena", 27, "senior SDET", 180_000),
+                new Tester ("Nasiba", 25, "QA engineer", 120_000 )
+        ));
+
+        System.out.println();
+
+
+        for(Tester each : testers){
+            System.out.println(each);
+            each.sleep(8);
+            System.out.println("====================");
+        }
 
 
 
