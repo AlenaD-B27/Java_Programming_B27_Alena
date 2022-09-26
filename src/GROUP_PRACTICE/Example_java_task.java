4babd1e8caa485124e60370a8edbf3fb345aa442
AlenaD-B27 <lenador.18@gmail.com>
2022-09-26 16:50:25 -0500

    Example of Festina's tasks

---
+++ src/GROUP_PRACTICE/Example_java_task.java
@@ -0,0 +1,45 @@
+package GROUP_PRACTICE;
+
+public class Example_java_task {
+
+    public static void methodName(){
+        // here you do the task
+        // your code here
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+        // main method OUTSIDE of the other method
+        // here you can call your method
+        methodName();
+    }
+
+
+                // division task:
+
+    // if you have 2 numbers:
+    // numberOne, numberTwo
+    // the method should return the result of numberOne divided ("/") by numberTwo.
+
+    // Example:
+    // int numberOne = 6;
+    // int numberTwo = 3;
+    // when we CALL the method instead of
+    // int result = numberOne/numberTwo;
+    // divide(numberOne, numberTwo);  => should return 2.
+
+                // FINRA:
+    // return numbers from 1 to 30, but:
+    // if number can be divided by 3 with no remainder (3,6,9) => instead of number it should print FIN;
+    // if number can be divided by 5 with no remainder (5,10) => instead of number it should print RA;
+    // if number can be divided by 3 AND 5 with no remainder (15,30) => instead of number it should print FINRA;
+    // Output: 1 2 FIN 4 RA FIN 7 8 FIN RA 11 FIN 13 14 FINRA ..... etc
+
+
+
+
+
+
+
+
+}
