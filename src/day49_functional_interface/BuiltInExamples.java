e9cd73706a63e9e9a55a39a928cf42071d742227
AlenaD-B27 <lenador.18@gmail.com>
2022-10-03 14:52:36 -0500

    Consumer

--- src/day49_functional_interface/BuiltInExamples.java
+++ src/day49_functional_interface/BuiltInExamples.java
@@ -3,13 +3,14 @@ package day49_functional_interface;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class BuiltInExamples {
     public static void main(String[] args) {
 
         // Predicate is a build in functional interface.
-        // Predicate has a test<T> method which returns boolean
+        // Predicate has a test(T t) method which returns boolean
 
         Predicate<Integer> isPrime = p -> {
 
@@ -35,6 +36,32 @@ public class BuiltInExamples {
         nums = new ArrayList<>(Arrays.asList(1, 5, 21, 6, 11, 12, 4, 15, 1, 2, 5, 1, 2, 4, 1, 5, 12, 3, 5, 12, 32, 5, 15, 2));
         nums.removeIf(p -> p < 10);
         System.out.println(nums);
+
+        System.out.println("------------------------------------");
+
+        // Consume is a build in functional interface.
+        // Predicate has a accept(T t) method which returns void
+
+        Consumer<int []> printArray = arr -> {
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " - ");
+            }
+        };
+
+        printArray.accept(new int[]{3, 6, 8});
+
+        List<String> words = new ArrayList<>(Arrays.asList("java", "soft skills", "house", "shed", "garden", "emoji", "fun"));
+        words.forEach(each -> System.out.println(each));
+
+        System.out.println("-----------------------------------------");
+
+        words.forEach(each -> {
+            System.out.println("" + each.charAt(0) + each.charAt(each.length() -1 )); // we want to print first and last chars
+            System.out.println(each);
+        });
+        System.out.println("======");
+
+        words.forEach(each -> System.out.println(each.substring(0, 3))); // print first 3 chars of each word
 
 
 
